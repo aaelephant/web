e3d72b08cdb54e441eb309814e294dd866dbb2e1
qbshen <qbshen_whaley>
2017-03-10 20:43:35 +0800

    add web test_ml_demo

---
+++ test_ml/test.js
@@ -0,0 +1,15 @@
+
+function init() {
+
+  const helo = document.querySelector('.helo');
+  helo.style.fontSize = '78px';
+  helo.style.color = '#79e';
+
+ }
+
+// window.onload = function() {
+//   init();
+// }
+window.addEventListener('load', function() { 
+  init();
+});
